9928adee02b9308ffadb4eb4371f29c5fc02adf7
msa <msai.sadehi@gmail.com>
2023-06-24 22:56:25 +0330

    sadeghi

--- session1.py
+++ session1.py
@@ -1,1 +1,89 @@
-print("hello every body")
+# print("hello every body")
+
+# x = 1.0
+# print(type(x))
+
+# x = 1 + 2j
+# print(type(x))
+
+# n = 'a'
+# n = "a"
+# n = "aaaaa"
+# n = ""
+# n = " "
+
+# message = 'he said "some thing"'
+# message = "he said \"some thing\""
+# message = '''he said "some thing"'''
+
+# x = 1
+# x_1 = 12
+# _x = 13
+# x_r_1 = 12
+
+# number_one = 12
+# numberOne = 14
+
+# name = "tina"
+# family = "rezaei"
+# message = "Hello %s %s"%(name, family)
+# message = f"Hello {name} {family}"
+# print(message)
+
+# name = "tina"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# name[0] = "S" # error
+
+# family = "rezaei"
+
+# message = "Hello " + name + " " +family
+# print(message)
+
+# print("*" * 10)
+
+
+# shooping_list = []
+# print(type(shooping_list))
+# print(shooping_list)
+# print(len(shooping_list))
+
+
+shooping_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(type(shooping_list))
+# print(shooping_list)
+# print(len(shooping_list))
+
+# print(shooping_list[0])
+# print(shooping_list[1])
+# print(shooping_list[-1])
+
+# TODO برنامه ای بنویس که جمع اعدا لیست بالا را محاسبه و نمایش دهد
+
+# slice
+# print(shooping_list[:3])
+
+# TODO برنامه ای بنویس که برش های زیر را از لیت بالا ایجاد نماید
+# [1,2]
+# [1,2,3]
+# [3,4]
+# [8,9]
+# [7,8,9]
+
+# my_list = [1, "a", "", "asd"]
+
+# numbers1 = [1, 2, 3, 4, 5]
+# numbers2 = [6, 7, 8, 9]
+# result = numbers1 + numbers2
+
+# print(result)
+
+# result = [numbers1, numbers2]
+# print(result)
+# برنامه ای بنویس که تک تک اعداد موجود در result
+# را پرینت نماید
+# همچنین تک تک اعداد موجود در
+# result
+# را با هم جمع نماید.
