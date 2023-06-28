83b4fc9746f6c8b2e50f8fbbd71d12190b0371c7
msa <msai.sadehi@gmail.com>
2023-06-28 16:57:49 +0330

    dragon

--- session1.py
+++ session1.py
@@ -51,26 +51,45 @@
 # print(len(shooping_list))
 
 
-shooping_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# print(type(shooping_list))
-# print(shooping_list)
-# print(len(shooping_list))
+shopping_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(type(shopping_list))
+# print(shopping_list)
+# print(len(shopping_list))
 
-# print(shooping_list[0])
-# print(shooping_list[1])
-# print(shooping_list[-1])
+# print(shopping_list[0])
+# print(shopping_list[1])
+# print(shopping_list[-1])
 
 # TODO برنامه ای بنویس که جمع اعدا لیست بالا را محاسبه و نمایش دهد
 
+# print(sum(shopping_list))
+
+# print(shopping_list[0] + shopping_list[1] + shopping_list[2] + shopping_list[3] +\
+#        shopping_list[4] + shopping_list[5] + shopping_list[6] + shopping_list[7] + shopping_list[8])
+# total = 0
+# for number in shopping_list:
+#     total += number # total = total + number
+# print(total)
+
+
 # slice
 # print(shooping_list[:3])
-
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # TODO برنامه ای بنویس که برش های زیر را از لیت بالا ایجاد نماید
 # [1,2]
+# print(numbers[:2])
+
 # [1,2,3]
+# print(numbers[:3])
 # [3,4]
+# print(numbers[2:4])
 # [8,9]
+# print(numbers[7:])
+# print(numbers[7:9])
 # [7,8,9]
+# print(numbers[6:])
+
+# print(numbers[-1])
 
 # my_list = [1, "a", "", "asd"]
 
@@ -87,3 +106,22 @@ shooping_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # همچنین تک تک اعداد موجود در
 # result
 # را با هم جمع نماید.
+
+# numbers1 = [1, 2, 3, 4, 5]
+# numbers2 = [6, 7, 8, 9]
+# result = [numbers1, numbers2]
+
+
+# result =[[1, 2, 3, 4, 5], [6, 7, 8, 9]]
+# print(result[0])
+# print(result[1])
+# print(result[0][0])
+# print(result[0][1])
+# print(result[0][2])
+# print(result[0][3])
+# print(result[0][4])
+
+# print(result[1][0])
+# print(result[1][1])
+# print(result[1][2])
+# print(result[1][3])
---
+++ session2.py
@@ -0,0 +1,18 @@
+# message = "hello every body. \nwelcome to python class. \nwe are going to learn every things about python"
+# print(message)
+# message = """hello every body. 
+# welcome to python class. 
+# we are going to learn every things about python"""
+# print(message)
+
+
+# message = "he said \"some thing\""
+# name = input("Enter your name:> ")
+# course = input("Enter the course name:> ")
+# message = f"hello {name}. welcome to {course} class."
+# print(message)
+
+# TODO   برنامه ای بنویس که نام و نام خانوادگی فردی را از ورودی دریافت نماید و  پیغامی مشابه زیر نمایش دهد
+# ali rezaei is a python developer
+
+print("sara" + " " +"rezaei")
