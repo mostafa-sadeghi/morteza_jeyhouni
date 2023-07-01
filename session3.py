c08214423d71596a3db63aa5f0218757db77f59e
msa <msai.sadehi@gmail.com>
2023-07-01 22:58:59 +0330

    sadeghi

---
+++ session3.py
@@ -0,0 +1,127 @@
+# print("hi " * 5)
+
+
+# shopping_list = ["egg", "potato", "pen", "book"]
+# for item in shopping_list:
+#     print(item)
+
+
+# new_item = input("enter new product: ")
+# shopping_list.append(new_item)
+
+# print(shopping_list)
+
+# print(shopping_list[0])
+
+# print(shopping_list[0:2])
+# print(shopping_list[:2])
+# print(shopping_list[1:3])
+
+
+# shopping_list = ["egg", "potato", "pen", "book"]
+
+# shopping_list.remove("book")
+# shopping_list.append("pencil")
+# shopping_list.pop()
+# shopping_list.pop(1)
+# del shopping_list[0]
+
+# print(shopping_list)
+
+# my_list = [1, "ali", [1, 2, 3], 1.5]
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[3])
+
+
+# x = 3
+# y = 4
+
+# z = x + y
+# print(z)
+# x = "3"
+# y = "4"
+
+# z = x + y
+# print(z)
+
+
+# x = [3]
+# y = [4]
+
+# z = x + y
+# print(z)
+
+
+# x = 3
+# y = 2
+
+# z = x * y
+# print(z)
+
+# x = "3"
+# y = 2
+
+# z = x * y
+# print(z)
+
+# x = [3]
+# y = 2
+
+# z = x * y
+# print(z)
+
+
+# tuple
+
+# x = (2,3)
+# print(type(x))
+
+# for number in x:
+#     print(number)
+
+
+# x = [2,3]
+
+# x[0] = 200
+# print(x)
+
+
+# x = (2, 3)
+
+# x[0] = 200  # error
+# print(x)
+
+
+# favorite_sports = ["football", "basketball", "tennis", "swimming"]
+
+# favorite_sports = {
+#     'sara': 'football',
+#     'roze': "basketball",
+#     "artin": "tennis",
+#     "reza": "swimming"
+# }
+
+
+# print(f"reza likes {favorite_sports['reza']}")
+# print(f"roze likes {favorite_sports['roze']}")
+# print(f"artin likes {favorite_sports['artin']}")
+# print(f"sara likes {favorite_sports['sara']}")
+
+
+# favorite_sports["armin"] = "football"
+# print(favorite_sports)
+
+# del favorite_sports['roze']
+# print(favorite_sports)
+
+# TODO با استفاده از دیکشنری برنامه ای بنویس که نام و نام خانوادگی و قد یک دانش آموز را از ورودی
+# دریافت نماید و در دیکشنری ذخیره کند
+# در نهایت دیکشنری را پرینت کن
+
+
+
+# TODO برنامه ای بنویس که سه عدد از ورودی دریافت نماید و در صورت زوج بودن در لیست اضافه کند
+
+# number % 2 == 0
