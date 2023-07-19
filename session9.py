bbbf2a4c96fa224dc92d63fd765735d93a6fb8a2
msa <msai.sadehi@gmail.com>
2023-07-19 16:58:03 +0330

    about for loop

---
+++ session9.py
@@ -0,0 +1,95 @@
+# for i in range(5):
+#     print("hello pygame")
+#
+# print("outside")
+#
+# for i in range(5):
+#     print(i, end=" ")
+
+
+# for x in range(1, 11):
+#     print(x, end=" ")
+#
+# for i in range(2, 11, 2):
+#     print(i, end=" ")
+# print()
+# for i in range(1,11,2):
+#     print(i, end=" ")
+#
+#
+# for i in range(10, 0, -1):
+#     print(i, end=" ")
+# print()
+# for i in range(10, -1, -1):
+#     print(i, end=" ")
+
+
+
+# for number in [2,3,4,5,6,7,8,9]:
+#     print(number)
+#
+# numbers = [1,2,3,4,5,6,7,8,9]
+# TODO
+"""
+با کمک حلقه فور اعداد موجود در لیست بالا را با هم جمع کن و حاصلجمع را نمایش بده
+"""
+
+
+# for i in range(3):
+#     print("a")
+# for j in range(3):
+#     print("b")
+# for i in range(3):
+#     print("a")
+#     for j in range(3):
+#         print("b")
+
+
+# TODO
+"""
+برنامه ای بنویسید که با کمک حلقه فور، چهار عدد از ورودی دریافت نماید و حاصل جمع آنها را محاسبه و نمایش دهد
+"""
+
+# total = 0
+# for i in range(3):
+#     new_number = int(input("enter a number: "))
+#     if new_number % 2 == 0 :
+#         total += new_number
+#
+# print("total is :", total)
+
+
+import turtle
+
+# for i in range(3):
+#     turtle.forward(100)
+#     turtle.left(120)
+
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.left(90)
+# for i in range(5):
+#     turtle.forward(100)
+#     turtle.left(72)
+
+# TODO
+
+"""
+کشیدن شش ضلعی و هفت ضلعی و هشت ضلعی
+"""
+turtle.fillcolor("red")
+turtle.begin_fill()
+turtle.circle(100)
+turtle.end_fill()
+
+# TODO
+
+"""
+کشیدن ستاره
+
+"""
+
+
+
+
+turtle.done()
